fdae5f056fe8a1b963cfd337848acee101d7a635
deadsidestar <sxycoup75@protonmail.com>
2018-04-22 10:23:33 -0700

    added urxvt-resize-font-git

--- 500-software-AUR-repo-distro-specific-v1.sh
+++ 500-software-AUR-repo-distro-specific-v1.sh
@@ -21,6 +21,8 @@ sh AUR-DS/install-perl-linux-desktopfiles-v*.sh
 
 sh AUR-DS/install-xfce-slimlock-v*.sh
 
+sh AUR-DS/install-urxvt-resize-font-git-v*.sh
+
 echo "################################################################"
 echo "####       Software from AUR-DS folder installed          ######"
 echo "################################################################"
