64a974b007b96b06a5a2557e67bb1dbbc06b2133
Nick Petrov <nickpetrov75@gmail.com>
2018-05-18 09:41:23 -0700

    added arcolinux-applications-git

--- 600-software-from-ArcoLinux-repo-v1.sh
+++ 600-software-from-ArcoLinux-repo-v1.sh
@@ -20,6 +20,7 @@ echo
 
 sudo pacman -S arcolinux-arc-themes-git --noconfirm --needed
 sudo pacman -S arcolinux-awesome-git --noconfirm --needed
+sudo pacman -S arcolinux-applications-git --noconfirm --needed
 #sudo pacman -S arcolinux-common-git --noconfirm --needed
 sudo pacman -S arcolinux-config-git --noconfirm --needed
 #sudo pacman -S arcolinux-conky-collection-git --noconfirm --needed
