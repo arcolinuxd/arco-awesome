50671273d9b96902e6bb2400a6394bd9164f1e4c
Erik Dubois <erik.dubois@gmail.com>
2018-05-30 13:32:27 +0200

    awesome logout

--- 920-fix-logout-v1.sh
+++ 920-fix-logout-v1.sh
@@ -13,14 +13,7 @@ set -e
 #
 ##################################################################################################################
 
-echo "Sardi icons applied when logging out or shutdown or ..."
-
-sudo sed -i 's/buttontheme = oxygen/buttontheme = Sardi-Oblogout/g' /etc/oblogout.conf
-#sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco'/g' /etc/oblogout.conf
-#sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco-mono'/g' /etc/oblogout.conf
-#sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco-cores'/g' /etc/oblogout.conf
-
-echo "changing the application to lock your system"
+echo "Setting pkill awesome to log out with arcolinux-oblogout"
 sudo sed -i 's/logout = openbox --exit/logout = pkill awesome/g' /etc/oblogout.conf
 
 echo "################################################################"
