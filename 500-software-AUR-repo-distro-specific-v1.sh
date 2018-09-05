8e6f0dfd1ef2c484124ced388881db945675792a
Erik Dubois <erik.dubois@gmail.com>
2018-09-05 11:06:54 +0200

    xfce-slimlock gone

--- 500-software-AUR-repo-distro-specific-v1.sh
+++ 500-software-AUR-repo-distro-specific-v1.sh
@@ -19,8 +19,6 @@ sh AUR-DS/install-gtk2-perl-v*.sh
 
 sh AUR-DS/install-perl-linux-desktopfiles-v*.sh
 
-sh AUR-DS/install-xfce-slimlock-v*.sh
-
 sh AUR-DS/install-urxvt-resize-font-git-v*.sh
 
 sh AUR-DS/install-python-pywal-v*.sh
