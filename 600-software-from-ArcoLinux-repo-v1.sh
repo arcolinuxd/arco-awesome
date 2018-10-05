7fc76608a3e107959c20a09133260fbbdc898e93
Erik Dubois <erik.dubois@gmail.com>
2018-10-05 20:31:18 +0200

    added arcolinux-mirrorlist-git to script 600

--- 600-software-from-ArcoLinux-repo-v1.sh
+++ 600-software-from-ArcoLinux-repo-v1.sh
@@ -39,6 +39,7 @@ sudo pacman -S arcolinux-lightdm-gtk-greeter --noconfirm --needed
 sudo pacman -S arcolinux-lightdm-gtk-greeter-settings --noconfirm --needed
 #sudo pacman -S arcolinux-local-git --noconfirm --needed
 sudo pacman -S arcolinux-neofetch-git --noconfirm --needed
+sudo pacman -S arcolinux-mirrorlist-git --noconfirm --needed
 sudo pacman -S arcolinux-nitrogen-git --noconfirm --needed
 sudo pacman -S arcolinux-oblogout-awesome --noconfirm --needed
 #sudo pacman -S arcolinux-oblogout --noconfirm --needed
