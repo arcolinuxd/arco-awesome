af7b723db00725d52835e771a10bf12412875891
Yves Simenya <>
2019-02-02 16:23:10 +0100

    Adapt makepkg.conf patch to machines with more than 16 cores

--- 000-use-all-cores-makepkg-conf-v3.sh
+++ 000-use-all-cores-makepkg-conf-v3.sh
@@ -15,52 +15,15 @@ set -e
 #
 ##################################################################################################################
 
-numberofcores=$(grep -c ^processor /proc/cpuinfo)
+N_CORES=$(grep -c ^processor /proc/cpuinfo)
 
+[[ $((N_CORES%2)) != 0 ]] && echo ":: Suspicious - odd - number of cores (${N_CORES}) ... I give up!" && exit 0
 
-case $numberofcores in
-
-    16)
-        echo "You have " $numberofcores" cores."
-        echo "Changing the makeflags for "$numberofcores" cores."
-        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j17"/g' /etc/makepkg.conf
-        echo "Changing the compression settings for "$numberofcores" cores."
-        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 16 -z -)/g' /etc/makepkg.conf
-        ;;
-    8)
-        echo "You have " $numberofcores" cores."
-        echo "Changing the makeflags for "$numberofcores" cores."
-        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j9"/g' /etc/makepkg.conf
-        echo "Changing the compression settings for "$numberofcores" cores."
-        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 8 -z -)/g' /etc/makepkg.conf
-        ;;
-    6)
-        echo "You have " $numberofcores" cores."
-        echo "Changing the makeflags for "$numberofcores" cores."
-        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j7"/g' /etc/makepkg.conf
-        echo "Changing the compression settings for "$numberofcores" cores."
-        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 6 -z -)/g' /etc/makepkg.conf
-        ;;
-    4)
-        echo "You have " $numberofcores" cores."
-        echo "Changing the makeflags for "$numberofcores" cores."
-        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j5"/g' /etc/makepkg.conf
-        echo "Changing the compression settings for "$numberofcores" cores."
-        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 4 -z -)/g' /etc/makepkg.conf
-        ;;
-    2)
-        echo "You have " $numberofcores" cores."
-        echo "Changing the makeflags for "$numberofcores" cores."
-        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j3"/g' /etc/makepkg.conf
-        echo "Changing the compression settings for "$numberofcores" cores."
-        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 2 -z -)/g' /etc/makepkg.conf
-        ;;
-    *)
-        echo "We do not know how many cores you have."
-        echo "Do it manually."
-        ;;
-
-esac
+echo ":: You have "$N_CORES" cores."
+echo ":: Changing the makeflags for "$N_CORES" cores."
+sudo sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$((N_CORES + 1))\"/g" /etc/makepkg.conf
+echo ":: Changing the compression settings for "$N_CORES" cores."
+sudo sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T ${N_CORES} -z -)/g" /etc/makepkg.conf
 
 echo "################################################################"
 echo "###  All cores will be used during building and compression ####"
