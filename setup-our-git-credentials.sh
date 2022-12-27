591ffdf8304b8878013afe2281a97117e72f5935
arcolinuxz <arcolinuxinfo@gmail.com>
2022-12-27 10:22:45 +0100

    update

--- setup-our-git-credentials.sh
+++ setup-our-git-credentials.sh
@@ -54,7 +54,7 @@ echo
 #https://blog.nillsf.com/index.php/2021/05/27/github-sso-using-password-protected-ssh-keys
 
 project=$(basename `pwd`)
-githubdir=$(basename $(dirname "$PWD"))
+githubdir=$(basename $(dirname "$PWD" | tr '[:upper:]' '[:lower:]'))
 echo "-----------------------------------------------------------------------------"
 echo "this is project https://github.com/$githubdir/$project"
 echo "-----------------------------------------------------------------------------"
